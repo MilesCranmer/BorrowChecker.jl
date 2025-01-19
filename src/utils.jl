4d0ee282bf34f73c5fcc0387134642bc44af09dc
MilesCranmer <miles.cranmer@gmail.com>
2025-01-19 12:37:12 +0000

    ci: skip known coverage

--- src/utils.jl
+++ src/utils.jl
@@ -3,7 +3,9 @@ module UtilsModule
 # Analogous to `nothing` but never used to mean something
 struct Unused end
 
+# COV_EXCL_START
 isunused(::Any) = false
 isunused(::Unused) = true
+# COV_EXCL_STOP
 
 end
