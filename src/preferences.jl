8bc21d6f496bca20191178fe6cb5d43eb50abfaa
MilesCranmer <miles.cranmer@gmail.com>
2025-01-20 00:22:43 +0000

    fix: cache collision with default UUID

--- src/preferences.jl
+++ src/preferences.jl
@@ -19,15 +19,15 @@ struct Cache{A,B}
     Cache{A,B}() where {A,B} = new{A,B}(Dict{A,B}(), Threads.SpinLock())  # COV_EXCL_LINE
 end
 
+const DEFAULT_UUID = Base.UUID(0)
+
 const UUID_CACHE = Cache{UInt64,Base.UUID}()
 const PREFERENCE_CACHE = Cache{Base.UUID,Tuple{Bool,IsCached}}()
 const MODULE_CACHE = Cache{Module,Bool}()
 
 function _cached_call(f::F, cache::Cache, key) where {F}
     lock(cache.lock) do
-        get!(cache.cache, key) do
-            f()
-        end
+        key == DEFAULT_UUID ? f() : get!(f, cache.cache, key)
     end
 end
 
@@ -36,7 +36,7 @@ function _cached_get_uuid(m)
         try
             get_uuid(m)
         catch
-            Base.UUID(0)
+            DEFAULT_UUID
         end
     end
 end
@@ -44,10 +44,10 @@ end
 function is_borrow_checker_enabled(calling_module)
     uuid = _cached_get_uuid(calling_module)
     (value, cached) = _cached_call(PREFERENCE_CACHE, uuid) do
-        if has_preference(uuid, "borrow_checker")
-            (load_preference(uuid, "borrow_checker"), Cached)
-        else
+        if uuid == DEFAULT_UUID || !has_preference(uuid, "borrow_checker")
             (true, NotCached)
+        else
+            (load_preference(uuid, "borrow_checker"), Cached)
         end
     end
     if cached == Cached
