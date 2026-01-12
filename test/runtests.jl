dcfc03ff8cc91e911e379aeaaddaeb5a2924d24a
MilesCranmer <miles.cranmer@gmail.com>
2026-01-12 15:16:44 +0000

    ci: run auto on 1.12 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,20 +22,27 @@ include("dynamic_expressions_integration_tests.jl")
 end
 
 @testitem "JET tests" begin
-    if VERSION >= v"1.10.0" && VERSION < v"1.12.0-DEV.0"
+    if VERSION >= v"1.10.0" && VERSION < v"1.13.0-DEV.0"
         test_jet_file = joinpath((@__DIR__), "test_jet.jl")
         run(`$(Base.julia_cmd()) --startup-file=no $test_jet_file`)
     end
 end
 
 const testitem_name_filter = get(ENV, "BORROWCHECKER_TESTITEM", "")
-const include_auto =
-    lowercase(get(ENV, "BORROWCHECKER_INCLUDE_AUTO", "")) in ("1", "true", "yes") ||
-    lowercase(get(ENV, "BORROWCHECKER_INCLUDE_EXPERIMENTAL", "")) in ("1", "true", "yes")
+const only_auto = lowercase(get(ENV, "BORROWCHECKER_ONLY_AUTO", "")) in ("1", "true", "yes")
+const auto_supported = VERSION >= v"1.12.0" && isdefined(Base, :code_ircode_by_type)
+
+if only_auto && !auto_supported
+    error("BORROWCHECKER_ONLY_AUTO requires Julia >= 1.12 with Base.code_ircode_by_type")
+end
 
 filter = if !isempty(testitem_name_filter)
-    ti -> ti.name == testitem_name_filter
-elseif !include_auto
+    ti ->
+        ti.name == testitem_name_filter &&
+            (auto_supported || !(:auto in ti.tags))
+elseif only_auto
+    ti -> :auto in ti.tags
+elseif !auto_supported
     ti -> !(:auto in ti.tags)
 else
     nothing
