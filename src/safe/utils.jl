2e3eb8e2f188d7d2ae958208af47ae654b72cc32
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-08-23 14:48:43 +0100

    rebase: content absorbed upstream (see #88)

---
+++ src/safe/utils.jl
@@ -0,0 +1,22 @@
+"""
+    PerTaskCache{T,F}
+
+A per-task cache that allows us to avoid repeated locking.
+"""
+struct PerTaskCache{T,F<:Function}
+    constructor::F
+
+    PerTaskCache{T}(constructor::F) where {T,F} = new{T,F}(constructor)
+end
+PerTaskCache{T}() where {T} = PerTaskCache{T}(() -> T())
+
+function Base.getindex(cache::PerTaskCache{T}) where {T}
+    tls = Base.task_local_storage()
+    if haskey(tls, cache)
+        return tls[cache]::T
+    else
+        value = cache.constructor()::T
+        tls[cache] = value
+        return value
+    end
+end
